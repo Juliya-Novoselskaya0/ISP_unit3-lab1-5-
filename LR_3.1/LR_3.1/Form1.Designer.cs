b28f1193777dad7dd750c25d627dad678686d0b7
Juliya <57349170+Juliya-Novoselskaya0@users.noreply.github.com>
2021-09-18 14:22:28 +0300

    Add files via upload

---
+++ LR_3.1/LR_3.1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace LR_3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelREZULT = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelREZ = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(498, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(348, 237);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 15);
+            this.lbl1.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(167, 247);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(132, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "начальная скорость V";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(215, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(342, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вычисление высоты подъема мяча";
+            this.label2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelREZULT
+            // 
+            this.labelREZULT.AutoSize = true;
+            this.labelREZULT.Location = new System.Drawing.Point(513, 147);
+            this.labelREZULT.Name = "labelREZULT";
+            this.labelREZULT.Size = new System.Drawing.Size(0, 15);
+            this.labelREZULT.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(80, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Line",
+            "Rectangle",
+            "FillRectangle",
+            "Ellipse",
+            "FillEllipse",
+            "Pie",
+            "FillPie"});
+            this.listBox1.Location = new System.Drawing.Point(40, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // labelREZ
+            // 
+            this.labelREZ.AutoSize = true;
+            this.labelREZ.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelREZ.Location = new System.Drawing.Point(428, 237);
+            this.labelREZ.Name = "labelREZ";
+            this.labelREZ.Size = new System.Drawing.Size(46, 25);
+            this.labelREZ.TabIndex = 9;
+            this.labelREZ.Text = "h = ";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Items.AddRange(new object[] {
+            "red",
+            "green",
+            "blue"});
+            this.listBox2.Location = new System.Drawing.Point(40, 137);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 34);
+            this.listBox2.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(40, 193);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "thickness";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.labelREZ);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelREZULT);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelREZULT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelREZ;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LR_3.1/LR_3.1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR_3._1
+{
+    public partial class Form1 : Form
+    {
+        static string path = "12345";
+        static Color color = Color.White;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+           // this.BackColor = Color.Pink;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //this.BackColor = Color.LightBlue;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text == path)
+            {
+                lbl1.Text = "Добро пожаловать!";
+                showCalculate();
+            }
+            else
+            {
+                lbl1.Text = "Вход запрещён!";    
+            }
+        }
+
+        void showCalculate()
+        {
+            lbl1.Visible = false;
+            label2.Visible = true;
+            label1.Visible = true;
+            trackBar1.Visible = true;
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int v = trackBar1.Value;
+           const int g = 10;
+            double rez = Math.Pow(v, 2) / 2 * g;
+            labelREZ.Text = "h = "+Convert.ToString(rez);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var currentDate = DateTime.Now;
+            var newYearDate = new DateTime(currentDate.Year + 1, 1, 1);
+            var toNewYear = newYearDate - currentDate;
+            label3.Text = $"До нового года осталось {toNewYear.Days} дней {toNewYear.Hours} часов {toNewYear.Minutes} минут {toNewYear.Seconds} секунд.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            colorDialog1.FullOpen = true;
+            // установка начального цвета для colorDialog
+            colorDialog1.Color = this.BackColor;
+
+            colorDialog1.ShowDialog();
+
+            this.BackColor = colorDialog1.Color;// установка цвета формы
+            Color color= colorDialog1.Color;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.BackColor = color;
+            Graphics g = this.CreateGraphics();   // создание графического объекта
+            Pen pn = new Pen(Color.Red, 1);
+            int thinkness = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            switch (listBox2.SelectedIndex)       // выбор и рисование примитива
+            {
+                case 0: pn = new Pen(Color.Red, thinkness); break;
+                case 1: pn = new Pen(Color.Green, 4); break;
+                case 2: pn = new Pen(Color.Blue, 4); break;
+            }
+            Brush br = new SolidBrush(Color.Green);  // создание кисти
+            g.Clear(SystemColors.Control);        // очистка области цветом формы
+            switch (listBox1.SelectedIndex)       // выбор и рисование примитива
+            {
+                case 0: g.DrawLine(pn, 170, 40, 350, 180); break;
+                case 1: g.DrawRectangle(pn, 170, 30, 250, 150); break;
+                case 2: g.FillRectangle(br, 170, 30, 250, 150); break;
+                case 3: g.DrawEllipse(pn, 170, 30, 250, 150); break;
+                case 4: g.FillEllipse(br, 170, 30, 250, 150); break;
+                case 5: g.DrawPie(pn, 170, 30, 200, 200, 180, 225); break;
+                case 6: g.FillPie(br, 170, 30, 150, 150, 0, 45); break;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
